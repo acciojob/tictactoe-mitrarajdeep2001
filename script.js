f2aa8e78d5a9c2ffe2d50e572b67f70c272a936c
Rajdeep Mitra <116889292+mitrarajdeep2001@users.noreply.github.com>
2023-04-28 19:00:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -49,12 +49,12 @@ boxList.forEach((element) => {
       turn = changeTurn();
       checkWin();
       if (isGameOver) {
-        const winner = turn === 'o' ? player1 : player2;
+        const winner = turn === 'o' ? 'player1' : 'player2';
         document.querySelector(
           '.message'
         ).innerText = `${winner}, congratulations you won!`;
       } else {
-        const player = turn === 'x' ? player1 : player2;
+        const player = turn === 'x' ? 'player1' : 'player2';
         document.querySelector('.message').innerText = `${player}, you're up`;
       }
     }
